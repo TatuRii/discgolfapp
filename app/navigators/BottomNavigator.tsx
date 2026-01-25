3fac37486cddfaa4ff6d25d6f5bdfcfe1dea425d
Tatu Riikonen <141812966+TatuRii@users.noreply.github.com>
2026-01-25 15:13:54 +0200

    Adds bottom navigation bar (#2)
    Implements a bottom navigation bar using react-navigation.
    
    This change introduces a bottom tab navigator to provide easy access to the main sections of the application: Home, Map, Profile, and More.
    
    Icons are displayed for each tab, with styling that adapts to the selected theme.
    
    Co-authored-by: Tatu Riikonen <tatu.riikonen@gmail.com>

--- app/navigators/BottomNavigator.tsx
+++ app/navigators/BottomNavigator.tsx
@@ -1,9 +1,14 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { TextStyle, ViewStyle } from "react-native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { House, Map, CircleUserRound, CircleEllipsis } from "lucide-react-native"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
 
 import { HomeScreen } from "@/screens/HomeScreen"
 import { MapScreen } from "@/screens/MapScreen"
 import { MoreScreen } from "@/screens/MoreScreen"
 import { ProfileScreen } from "@/screens/ProfileScreen"
+import { useAppTheme } from "@/theme/context"
+import type { ThemedStyle } from "@/theme/types"
 
 export type BottomNavigatorParamList = {
   Home: undefined
@@ -12,16 +17,79 @@ export type BottomNavigatorParamList = {
   More: undefined
 }
 
-const Tab = createNativeStackNavigator<BottomNavigatorParamList>()
+const Tab = createBottomTabNavigator<BottomNavigatorParamList>()
+
 export const BottomNavigator = () => {
+  const { bottom } = useSafeAreaInsets()
+  const {
+    themed,
+    theme: { colors },
+  } = useAppTheme()
+
   return (
     <Tab.Navigator
-      screenOptions={{ contentStyle: { backgroundColor: "transparent" }, headerShown: false }}
+      screenOptions={{
+        headerShown: false,
+        tabBarHideOnKeyboard: true,
+        tabBarStyle: themed([$tabBar, { height: bottom + 70 }]),
+        tabBarActiveTintColor: colors.text,
+        tabBarInactiveTintColor: colors.text,
+        tabBarLabelStyle: themed($tabBarLabel),
+        tabBarItemStyle: themed($tabBarItem),
+      }}
     >
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Map" component={MapScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
-      <Tab.Screen name="More" component={MoreScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <House color={focused ? colors.tint : colors.tintInactive} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Map"
+        component={MapScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Map color={focused ? colors.tint : colors.tintInactive} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="More"
+        component={MoreScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <CircleEllipsis color={focused ? colors.tint : colors.tintInactive} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <CircleUserRound color={focused ? colors.tint : colors.tintInactive} size={24} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   )
 }
+
+const $tabBar: ThemedStyle<ViewStyle> = ({ colors }) => ({
+  backgroundColor: colors.background,
+  borderTopColor: colors.transparent,
+})
+
+const $tabBarItem: ThemedStyle<ViewStyle> = ({ spacing }) => ({
+  paddingTop: spacing.md,
+})
+
+const $tabBarLabel: ThemedStyle<TextStyle> = ({ colors, typography }) => ({
+  fontSize: 12,
+  fontFamily: typography.primary.medium,
+  lineHeight: 16,
+  color: colors.text,
+})
