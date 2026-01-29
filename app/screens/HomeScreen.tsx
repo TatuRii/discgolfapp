8e08b0daeee8b854eac520cf602fc5e0552b231f
Tatu Riikonen <141812966+TatuRii@users.noreply.github.com>
2026-01-29 15:20:16 +0200

    DGA-5 Add button for home screen (#5)
    -Added button to home screen where you can start new round
    -Added styles to the button and when its clicked
    
    Co-authored-by: Tatu Riikonen <tatu.riikonen@gmail.com>

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,9 +1,12 @@
 import { FC } from "react"
 import { ViewStyle } from "react-native"
 
+import { Button } from "@/components/Button"
 import { Screen } from "@/components/Screen"
 import { Text } from "@/components/Text"
 import type { AppStackScreenProps } from "@/navigators/navigationTypes"
+import { useAppTheme } from "@/theme/context"
+import type { ThemedStyle } from "@/theme/types"
 // import { useNavigation } from "@react-navigation/native"
 
 interface HomeScreenProps extends AppStackScreenProps<"Home"> {}
@@ -11,9 +14,20 @@ interface HomeScreenProps extends AppStackScreenProps<"Home"> {}
 export const HomeScreen: FC<HomeScreenProps> = () => {
   // Pull in navigation via hook
   // const navigation = useNavigation()
+  const { themed } = useAppTheme()
+
+  const handleClick = () => {
+    // navigation.navigate("SomeOtherScreen")
+  }
   return (
     <Screen style={$root} preset="scroll" safeAreaEdges={["top"]}>
       <Text text="Home" preset="heading" />
+      <Button
+        style={themed($button)}
+        text="Start new round"
+        onPress={handleClick}
+        pressedStyle={themed($buttonPressed)}
+      />
     </Screen>
   )
 }
@@ -21,3 +35,11 @@ export const HomeScreen: FC<HomeScreenProps> = () => {
 const $root: ViewStyle = {
   flex: 1,
 }
+
+const $button: ThemedStyle<ViewStyle> = ({ colors, spacing }) => ({
+  backgroundColor: colors.palette.primary500,
+  marginTop: spacing.md,
+})
+const $buttonPressed: ThemedStyle<ViewStyle> = ({ colors }) => ({
+  backgroundColor: colors.palette.neutral900,
+})
