e984199427053da3636b51f7376abcd95520adac
Tatu Riikonen <141812966+TatuRii@users.noreply.github.com>
2026-02-04 14:05:25 +0200

    DGA-8 Adds a practice button to the More screen (#7)
    Adds a button to the More screen that will navigate the user to the putt practice screen, alongside some basic styling.
    
    Co-authored-by: Tatu Riikonen <tatu.riikonen@gmail.com>

--- app/screens/MoreScreen.tsx
+++ app/screens/MoreScreen.tsx
@@ -1,9 +1,12 @@
 import { FC } from "react"
 import { ViewStyle } from "react-native"
 
+import { Button } from "@/components/Button"
 import { Screen } from "@/components/Screen"
 import { Text } from "@/components/Text"
 import type { AppStackScreenProps } from "@/navigators/navigationTypes"
+import { useAppTheme } from "@/theme/context"
+import type { ThemedStyle } from "@/theme/types"
 // import { useNavigation } from "@react-navigation/native"
 
 interface MoreScreenProps extends AppStackScreenProps<"More"> {}
@@ -11,12 +14,23 @@ interface MoreScreenProps extends AppStackScreenProps<"More"> {}
 export const MoreScreen: FC<MoreScreenProps> = () => {
   // Pull in navigation via hook
   // const navigation = useNavigation()
+  const { themed } = useAppTheme()
   return (
     <Screen style={$root} preset="scroll" safeAreaEdges={["top"]}>
-      <Text text="more" />
+      <Text preset="heading" text="More" />
+      <Text preset="subheading" text="Practice" />
+      <Button style={themed($button)} pressedStyle={themed($buttonPressed)} text="Putt practice" />
     </Screen>
   )
 }
+
+const $button: ThemedStyle<ViewStyle> = ({ colors, spacing }) => ({
+  backgroundColor: colors.tint,
+  marginTop: spacing.md,
+})
+const $buttonPressed: ThemedStyle<ViewStyle> = ({ colors }) => ({
+  backgroundColor: colors.tintInactive,
+})
 
 const $root: ViewStyle = {
   flex: 1,
